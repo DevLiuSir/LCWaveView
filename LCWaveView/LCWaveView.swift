35164ba08984fff93c5c6a7ac1ab7df3d90d8a63
ChinaHackers <93428739@qq.com>
2018-05-23 02:04:06 +0800

    ⚠️ Optimize your code, Fix the bug.

--- LCWaveView/LCWaveView.swift
+++ LCWaveView/LCWaveView.swift
@@ -9,48 +9,23 @@
 
 import UIKit
 
+// 波浪曲线动画视图
+// 波浪曲线公式：y = h * sin(a * x + b);
+//      h: 波浪高度， a: 波浪宽度系数， b： 波动的偏移量
 
 /// 波浪视图
 class LCWaveView: UIView {
-
-    /// 波动频率
-    public var waveFrequency: CGFloat = 1.5
-   
-    /// 波动速度
-    public var waveSpeed: CGFloat = 0.6
     
-    /// 波动高度
+    // MARK: - 公开属性
+
+    /// 波浪宽度系数 -> a
+    public var waveRate: CGFloat = 1.5
+    
+    /// 波动速度(默认值:0.5 取值 0 ～ 1)
+    public var waveSpeed: CGFloat = 0.5
+    
+    /// 波动的高度 -> h (默认值: 5)
     public var waveHeight: CGFloat = 5
-    
-    /// 波动视图之上的视图(头像)
-    public var overView: UIView?
-    
-    /// 真实波动图层
-    fileprivate var realWaveLayer: CAShapeLayer = CAShapeLayer()
-   
-    /// 蒙版波动图层
-    fileprivate var maskWaveLayer: CAShapeLayer = CAShapeLayer()
-    
-    /// 时间
-    fileprivate var timer: CADisplayLink?
-    
-    /// 边距
-    fileprivate var offset: CGFloat = 0
-    
-    /// 频率
-    fileprivate var priFrequency: CGFloat = 0
-    
-    /// 速度
-    fileprivate var priWaveSpeed: CGFloat = 0
-    
-    /// 高度
-    fileprivate var priWaveHeight: CGFloat = 0
-    
-    /// 开始
-    fileprivate var isStarting: Bool = false
-
-    /// 停止
-    fileprivate var isStopping: Bool = false
     
     /// 真实波动图层颜色
     public var realWaveColor: UIColor = UIColor.white {
@@ -59,32 +34,75 @@ class LCWaveView: UIView {
         }
     }
     /// 蒙版波动图层颜色
-    public var maskWaveColor: UIColor = UIColor.orange {
+    public var maskWaveColor: UIColor = UIColor.white {
         didSet {
             maskWaveLayer.fillColor = maskWaveColor.cgColor
         }
     }
-   
     
+    /// 波动完成回调
+    public var completion: ((_ centerY: CGFloat)->())?
+    
+    
+    // MARK: - 私有属性
+    
+    /// 真实波动图层
+    private lazy var realWaveLayer: CAShapeLayer = CAShapeLayer()
+   
+    /// 蒙版波动图层
+    private lazy var maskWaveLayer: CAShapeLayer = CAShapeLayer()
+    
+    /// 屏幕刷新率定时器
+    private var waveDisplayLink: CADisplayLink?
+    
+    /// 波浪的偏移量 -> b
+    private var offset: CGFloat = 0
+    
+    /// 频率
+    private var priFrequency: CGFloat = 0
+
+    /// 速度
+    private var priWaveSpeed: CGFloat = 0
+
+    /// 高度
+    private var priWaveHeight: CGFloat = 0
+    
+    /// 定义变量记录波动视图的状态 -开始（默认为:false）
+    private var isStarting: Bool = false
+
+    /// 定义变量记录波动视图的状态 -停止（默认为:false)
+    private var isStopping: Bool = false
+    
+   
     // MARK: - 初始化
     override init(frame: CGRect) {
         super.init(frame: frame)
-        var f = self.bounds
-        f.origin.y = frame.size.height
-        f.size.height = 0
-        maskWaveLayer.frame = f
-        realWaveLayer.frame = f
-        self.backgroundColor = .clear
-        self.layer.addSublayer(realWaveLayer)
-        self.layer.addSublayer(maskWaveLayer)
         
+        var tempf = bounds
+        tempf.origin.y = frame.size.height
+        tempf.size.height = 0
+        
+        maskWaveLayer.frame = tempf
+        realWaveLayer.frame = tempf
+        
+        backgroundColor = .clear
+        layer.addSublayer(realWaveLayer)
+        layer.addSublayer(maskWaveLayer)
     }
+    
     // MARK: - 便利构造器
+    
+    /// 初始化波浪视图的尺寸位置,以及颜色
+    ///
+    /// - Parameters:
+    ///   - frame: 尺寸位置
+    ///   - color: 颜色
     convenience init(frame: CGRect, color: UIColor) {
         self.init(frame: frame)
+        
         realWaveColor = color
         maskWaveColor = color.withAlphaComponent(0.7)
-        realWaveLayer.fillColor = realWaveColor.cgColor
+        realWaveLayer.fillColor = realWaveColor.cgColor     // 图层的填充颜色
         maskWaveLayer.fillColor = maskWaveColor.cgColor
     }
     
@@ -96,25 +114,24 @@ class LCWaveView: UIView {
 // MARK: - Method
 extension LCWaveView {
     
-    /// 添加波动视图之上的视图(头像)
-    public func addOverView(oView: UIView) {
-        overView = oView
-        overView?.center = self.center
-        overView?.frame.origin.y = self.frame.height - (overView?.frame.height)!
-        self.addSubview(overView!)
-    }
-    
     /// 开始波动
     public func startWave() {
+        
         if !isStarting {
-            removeTimer()
+            
+            removeTimer()   // 先移除屏幕刷新率定时器
             isStarting = true
             isStopping = false
-            priWaveHeight = 0
-            priFrequency = 0
-            priWaveSpeed = 0
-            timer = CADisplayLink(target: self, selector: #selector(waveEvent))
-            timer?.add(to: .current, forMode: .commonModes)
+            
+    /*   CADisplayLink:一个和屏幕刷新率相同的定时器，需要以特定的模式注册到runloop中，每次屏幕刷新时，会调用绑定的target上的selector这个方法。
+         duration:每帧之间的时间
+         pause:暂停，设置true为暂停，false为继续
+         结束时，需要调用invalidate方法，并且从runloop中删除之前绑定的target跟selector。
+         不能被继承
+     */
+            // 开启定时器
+            waveDisplayLink = CADisplayLink(target: self, selector: #selector(waveEvent))
+            waveDisplayLink?.add(to: .current, forMode: RunLoop.Mode.common)
         }
     }
     
@@ -128,20 +145,38 @@ extension LCWaveView {
     
     /// 移除定时器
     private func removeTimer() {
-        guard timer != nil else {return}
-        timer?.invalidate() // 从运行循环中移除定时器
-        timer = nil
+        // 从运行循环中移除定时器
+        waveDisplayLink?.invalidate()
+        waveDisplayLink = nil
     }
     
+    /// 浮动事件
+    @objc func waveEvent() {
+        
+
+        if isStarting {
+            BeganToWave()
+        }
+        if isStopping {
+            endToWave()
+        }
+        other()
+    }
+    
+}
+
+
+// MARK: - 浮动的三种状态（开始、结束、其他）
+extension LCWaveView {
     
     /// 开始波动起来
-    private func BeganToWave () {
+    private func BeganToWave() {
         guard priWaveHeight < waveHeight else {
             isStarting = false
             return
         }
         priWaveHeight = priWaveHeight + waveHeight / 100
-      
+        
         // 1.用一个临时变量,保存当前视图的尺寸
         var f = self.bounds
         
@@ -152,12 +187,12 @@ extension LCWaveView {
         // 3.修改frame的值
         maskWaveLayer.frame = f
         realWaveLayer.frame = f
-        priFrequency = priFrequency + waveFrequency / 100
+        priFrequency = priFrequency + waveRate / 100
         priWaveSpeed = priWaveSpeed + waveSpeed / 100
     }
     
     /// 结束波动
-    private func endToWave () {
+    private func endToWave() {
         guard priWaveHeight > 0 else {  // 停止
             isStopping = false
             stopWave()
@@ -175,24 +210,26 @@ extension LCWaveView {
         // 3.修改frame的值
         maskWaveLayer.frame = f
         realWaveLayer.frame = f
-        priFrequency = priFrequency - waveFrequency / 50.0
+        priFrequency = priFrequency - waveRate / 50.0
         priWaveSpeed = priWaveSpeed - waveSpeed / 50.0
     }
     
     /// 其他情况
     private func other() {
         
+        // 波浪移动的关键：按照指定的速度偏移
         offset += priWaveSpeed
+        
         var y: CGFloat = 0.0
         let width: CGFloat = frame.width
         let height: CGFloat = priWaveHeight
         
-        // 创建路径1,2
-        let path = CGMutablePath()
+        // 创建可变图形路径1、2
+        let realPath = CGMutablePath()
         let maskPath = CGMutablePath()
         
         // 开始指定一个新的子路径。
-        path.move(to: CGPoint(x: 0, y: height))
+        realPath.move(to: CGPoint(x: 0, y: height))
         maskPath.move(to: CGPoint(x: 0, y: height))
         
         let offset_f = Float(offset * 0.045)
@@ -200,40 +237,34 @@ extension LCWaveView {
         let waveFrequency_f = Float(0.01 * priFrequency)
         
         for x in 0...Int(width) {
-            y = height * CGFloat(sinf(waveFrequency_f * Float(x) + offset_f))
+            
+            // 波浪曲线
+            y = height * CGFloat(sin(waveFrequency_f * Float(x) + offset_f))
+            
             // 把这些点用先的形式绘制路径
-            path.addLine(to: CGPoint(x: CGFloat(x), y: y))
+            realPath.addLine(to: CGPoint(x: CGFloat(x), y: y))
             maskPath.addLine(to: CGPoint(x: CGFloat(x), y: -y))
         }
         
-        guard overView != nil else {
-            // 1.从当前点到指定点, 用线的形式绘制路径
-            path.addLine(to: CGPoint(x: width, y: height))
-            path.addLine(to: CGPoint(x: 0, y: height))
-            maskPath.addLine(to: CGPoint(x: width, y: height))
-            maskPath.addLine(to: CGPoint(x: 0, y: height))
-            // 2.关闭路径
-            maskPath.closeSubpath()
-            path.closeSubpath()
-            // 3.赋值路径
-            realWaveLayer.path = path
-            maskWaveLayer.path = maskPath
-            return
+        let midX = bounds.size.width * 0.5
+        let midY = height * sin(midX * CGFloat(waveFrequency_f) + CGFloat(offset_f))
+        
+        if let callback = completion {
+            callback(midY)
         }
-        let centX = self.bounds.size.width / 2
-        let centY = height * CGFloat(sinf(waveFrequency_f * Float(centX) + offset_f))
-        let center = CGPoint(x: centX , y: centY + self.bounds.size.height - overView!.bounds.size.height/2 - priWaveHeight - 1 )
-        overView?.center = center
+        
+        // 1.从当前点到指定点, 用线的形式绘制路径
+        realPath.addLine(to: CGPoint(x: width, y: height))
+        realPath.addLine(to: CGPoint(x: 0, y: height))
+        maskPath.addLine(to: CGPoint(x: width, y: height))
+        maskPath.addLine(to: CGPoint(x: 0, y: height))
+        // 2.关闭路径
+        maskPath.closeSubpath()
+        realPath.closeSubpath()
+        // 3.赋值路径
+        realWaveLayer.path = realPath
+        maskWaveLayer.path = maskPath
     }
     
-    /// 浮动事件
-    @objc func waveEvent() {
-        if isStarting {
-            BeganToWave()
-        }
-        if isStopping {
-            endToWave()
-        }
-        other()
-    }
 }
+
--- LCWaveViewExample/LCWaveViewExample/ViewController.swift
+++ LCWaveViewExample/LCWaveViewExample/ViewController.swift
@@ -8,33 +8,95 @@
 
 import UIKit
 
-/// 屏幕宽
+/// 屏幕的宽度
 private let screenW = UIScreen.main.bounds.width
 
-class ViewController: UIViewController {
+/// 头像视图的宽度
+private let iconImageWidth: CGFloat = 100
 
+/// 波动视图的高度
+private let waveViewHeight: CGFloat = 200
+
+/// 单元格重用标识符
+private let identifier = "cellID"
+
+
+
+class ViewController: UIViewController {
+    
+    /// 表格
+    @IBOutlet weak var table: UITableView!
+    
+    // MARK: - Lazy Loading
+    
+    /** LCWaveView **/
+    private lazy var waveView: LCWaveView = {
+        let waveView = LCWaveView(frame: CGRect(x: 0, y: 0, width: screenW, height: waveViewHeight), color: .white)
+        waveView.waveRate = 2
+        waveView.waveSpeed = 1
+        waveView.waveHeight = 7
+        return waveView
+    }()
+    
+    /// 头像视图
+    private lazy var iconImageView: UIImageView = {
+        let image = UIImageView(frame: CGRect(x: screenW / 2 - iconImageWidth / 2, y: 0, width: iconImageWidth, height: iconImageWidth))
+        image.layer.borderColor  = UIColor.white.cgColor
+        image.layer.cornerRadius = image.bounds.width / 2
+        image.layer.masksToBounds = true
+        image.layer.borderWidth = 3
+        image.layer.contents = UIImage(named: "UserAvatar.png")?.cgImage
+        return image
+    }()
+    
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // overView
-        let overView = UIImageView(frame: CGRect(x: 0, y: 0, width: 60, height: 60))
-        overView.layer.cornerRadius = overView.bounds.height / 2
-        overView.layer.masksToBounds = true
-        overView.layer.borderColor  = UIColor.white.cgColor
-        overView.layer.borderWidth = 3
-        overView.layer.contents = UIImage(named: "yourtion")?.cgImage
-        
-        
-        /** LCWaveView **/
-        let frame = CGRect(x: 0, y: 0, width: screenW, height: 200)
-        let waveView = LCWaveView(frame: frame, color: .white)
-        waveView.backgroundColor = .red
-        waveView.waveFrequency = 2
-        waveView.waveSpeed = 1
-        view.addSubview(waveView)
-//        waveView.addOverView(oView: overView)
+        configUI()
+    }
+}
+
+// MARK: - Custom Method
+extension ViewController {
+    
+    /// 配置UI界面
+    private func configUI() {
+        configTableView()
+        configWaveView()
+    }
+    
+    /// 配置波动视图
+    private func configWaveView() {
+        waveView.completion = { centerY in  // 波浪动画回调
+            // 同步更新头像视图的y坐标
+            self.iconImageView.frame.origin.y = waveViewHeight + centerY - iconImageWidth
+        }
+        waveView.addSubview(iconImageView)
         waveView.startWave()
     }
     
+    /// 配置表格
+    private func configTableView() {
+        table.backgroundColor = .red
+        table.dataSource = self
+        table.register(UITableViewCell.self, forCellReuseIdentifier: identifier)
+        table.contentInset = UIEdgeInsets(top: 100, left: 0, bottom: 0, right: 0)
+        table.tableHeaderView = waveView
+    }
 }
 
+// MARK: - UITableViewDataSource
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
+}
